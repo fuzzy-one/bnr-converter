342e6bbeb3dec55e59558ecdda2012e20b5f1d4b
Vlad Petrescu <vladxpetrescu@gmail.com>
2024-09-06 00:36:56 +0300

    try to fix PWA install

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,50 @@
-const cacheName = 'currency-converter-cache-v1';
-const assetsToCache = [
-  '/index.html',
-  '/styles.css',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
+const CACHE_NAME = 'currency-converter-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/manifest.json',
+    '/nbrfxrates.xml',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
 ];
 
-// Install event - Cache resources
+// Install event - cache necessary files
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(cacheName)
-      .then((cache) => {
-        console.log('Caching assets...');
-        return cache.addAll(assetsToCache);
-      })
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
-// Fetch event - Serve cached resources
+// Fetch event - serve files from cache if available, otherwise fetch from network
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        return response || fetch(event.request);
-      })
-  );
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                // Return the cached response if found, otherwise fetch from the network
+                return response || fetch(event.request);
+            })
+    );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        // Delete old cache
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
